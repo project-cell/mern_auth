93a77faf53bba0a4b91728af0aece1ef2b87bf12
pihu <pihuc250@gmail.com>
2024-09-03 13:40:37 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React from "react";//importing
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import Header from './components/header.jsx';
+
+
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter >
+  {/*header*/}
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+  </Routes>
+  </BrowserRouter>;
 }
+
